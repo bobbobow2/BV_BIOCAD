33b4a3e64a951606ee657ee989e8bd20ad987080
Murkat-git <murkat2007@yandex.ru>
2024-07-19 06:37:48 +0000

    Code refactor + fix cdr masks

--- optimizers/gradient.py
+++ optimizers/gradient.py
@@ -1,34 +1,29 @@
+from glob import glob
+
+import antiberty
+import igfold
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 import transformers
-
-from tqdm.notebook import tqdm
-
-import antiberty
 from antiberty import AntiBERTy
-
-from glob import glob
-import igfold
-# from utils.scorer import kabsch_mse, do_kabsch, kabsch
-
 from igfold.model.IgFold import IgFold
-from igfold.utils.folding import fold
-from igfold.utils.embed import embed
-from igfold.training.utils import do_kabsch, kabsch_mse
-from igfold.utils.general import exists
-
 from igfold.model.interface import IgFoldInput
+from igfold.training.utils import do_kabsch, kabsch_mse
+from igfold.utils.embed import embed
 from igfold.utils.fasta import get_fasta_chain_dict
+from igfold.utils.folding import fold
 from igfold.utils.general import exists
-from igfold.utils.pdb import get_atom_coords, save_PDB, write_pdb_bfactor, cdr_indices
-
+from igfold.utils.pdb import cdr_indices, get_atom_coords, save_PDB, write_pdb_bfactor
 from torch.utils.tensorboard import SummaryWriter
+from tqdm.notebook import tqdm
 
 from utils.general import *
 
-species_loss_fn = nn.CrossEntropyLoss()
+# from utils.scorer import kabsch_mse, do_kabsch, kabsch
+
+
+species_loss_fn = nn.CrossEntropyLoss(label_smoothing=0.15)
 
 
 def RMSD_loss_fn(preds, target, mask):
@@ -40,54 +35,90 @@ def RMSD_loss_fn(preds, target, mask):
     mse = F.mse_loss(
         preds,
         aligned_target,
-        reduction='none',
-    ).mean(-1)
+        reduction="none",
+    ).sum(-1)
+    # mse = torch.sum(
+    #         mse * mask,
+    #         dim=-1,
+    #     ).sum() / mask.mean(-1).sum()
+    # return mse
 
-    mse = torch.sum(
-            mse * mask,
-            dim=-1,
-        ).sum() / mask.mean(-1).sum()
-    
-    return mse
+    rmsd_sq = torch.sum(mse * mask, dim=(-2, -1)) / mask.mean(-1).sum()
+
+    return rmsd_sq
+
+
+def v_gene_identity(sequence):
+    c = Chain(sequence, scheme="imgt")
+    v_germline_chains, _ = c.find_human_germlines(limit=1)
+    top_v = v_germline_chains[0]
+
+    num_v_matches = sum(top_v.positions.get(pos) == aa for pos, aa in c)
+
+    v_identity = num_v_matches / len(top_v)
+    return v_identity
 
 
 device = torch.device("cuda")
+
+
 class GenAB(nn.Module):
     def __init__(self, b: FVbinder, sub_infinity=10):
         super().__init__()
-        seqs = [b.vh, b.vl]
+        seqs = [b.vh.seq, b.vl.seq]
         seqs = [" ".join(list(j)) for j in seqs]
 
         self.antiberty = AntiBERTy.from_pretrained(ANTIBERTY_CHECKPOINT_PATH).to(device)
         self.antiberty.eval()
-        
-        self.antiberty_tokenizer = transformers.BertTokenizer(vocab_file=ANTIBERTY_VOCAB_FILE, do_lower_case=False)
+
+        self.antiberty_tokenizer = transformers.BertTokenizer(
+            vocab_file=ANTIBERTY_VOCAB_FILE, do_lower_case=False
+        )
 
         self.tokenizer_out = self.antiberty_tokenizer(
-                seqs,
-                return_tensors="pt",
-                padding=True,
+            seqs,
+            return_tensors="pt",
+            padding=True,
         ).to(device)
         self.attention_mask = self.tokenizer_out["attention_mask"].to(device)
 
-        self.logits = F.one_hot(self.tokenizer_out.input_ids, num_classes=25).float() * sub_infinity
+        self.logits = (
+            F.one_hot(self.tokenizer_out.input_ids, num_classes=25).float()
+            * sub_infinity
+        )
 
         # Preprocess logits
-        vh_cdr_mask = F.pad(b.vh_cdr_mask.max(dim=1).values, pad=(1, self.logits.size(dim=1) - b.vh_cdr_mask.size(dim=0) - 1), mode="constant", value=0)
-        self.logits[0][vh_cdr_mask==1] = self.logits[0][vh_cdr_mask==1].where(self.logits[0][vh_cdr_mask==1]!=0, torch.tensor([-np.inf], device=device))
-        
-        vl_cdr_mask = F.pad(b.vl_cdr_mask.max(dim=1).values, pad=(1, self.logits.size(dim=1) - b.vl_cdr_mask.size(dim=0) - 1), mode="constant", value=0)
-        self.logits[1][vl_cdr_mask==1] = self.logits[1][vl_cdr_mask==1].where(self.logits[1][vl_cdr_mask==1]!=0, torch.tensor([-np.inf], device=device))
+        vh_cdr_mask = F.pad(
+            b.vh_cdr_mask.max(dim=1).values,
+            pad=(1, self.logits.size(dim=1) - b.vh_cdr_mask.size(dim=0) - 1),
+            mode="constant",
+            value=0,
+        )
+        self.logits[0][vh_cdr_mask == 1] = self.logits[0][vh_cdr_mask == 1].where(
+            self.logits[0][vh_cdr_mask == 1] != 0,
+            torch.tensor([-np.inf], device=device),
+        )
 
-        self.logits[:, :, 0:5][self.logits[:, :, 0:5]==0] = -np.inf
+        vl_cdr_mask = F.pad(
+            b.vl_cdr_mask.max(dim=1).values,
+            pad=(1, self.logits.size(dim=1) - b.vl_cdr_mask.size(dim=0) - 1),
+            mode="constant",
+            value=0,
+        )
+        self.logits[1][vl_cdr_mask == 1] = self.logits[1][vl_cdr_mask == 1].where(
+            self.logits[1][vl_cdr_mask == 1] != 0,
+            torch.tensor([-np.inf], device=device),
+        )
+
+        self.logits[:, :, 0:5][self.logits[:, :, 0:5] == 0] = -np.inf
         for b in range(self.logits.size(dim=0)):
             for i in range(self.logits.size(dim=1)):
                 if 0 <= self.logits[b, i].argmax() <= 4:
-                    self.logits[b, i, self.logits[b, i, :]==0] = -np.inf
-        
+                    self.logits[b, i, self.logits[b, i, :] == 0] = -np.inf
+
         self.logits = nn.Parameter(self.logits)
         self.logits.requires_grad = True
-        
+
         self.W_matrix = self.antiberty.bert.embeddings.word_embeddings.weight
         self.antiberty.bert.embeddings.word_embeddings = nn.Identity()
 
@@ -96,30 +127,27 @@ class GenAB(nn.Module):
             # print(f"Loading {ckpt_file}...")
             m = IgFold.load_from_checkpoint(ckpt_file).eval().to(device)
             self.models.append(m)
-            
+
     def forward(self):
         w_embeddings = F.softmax(self.logits, dim=-1) @ self.W_matrix
         embeddings = self.antiberty.bert.embeddings(
-            input_ids = w_embeddings,
-            token_type_ids = self.tokenizer_out.token_type_ids
+            input_ids=w_embeddings, token_type_ids=self.tokenizer_out.token_type_ids
         )
 
         outputs = self.antiberty(
             inputs_embeds=embeddings,
             attention_mask=self.attention_mask,
             output_hidden_states=True,
-            output_attentions=True
+            output_attentions=True,
         )
 
-        #Pseudo Log-Likelyhood
+        # Pseudo Log-Likelyhood
         m_logits = self.logits
-        
+
         pred_logits = outputs.prediction_logits
-        
+
         log_likelyhood = -F.cross_entropy(
-            pred_logits,
-            F.softmax(m_logits, dim=-1),
-            reduction="mean"
+            pred_logits, F.softmax(m_logits, dim=-1), reduction="mean"
         )
 
         # Coords Preds
@@ -129,7 +157,7 @@ class GenAB(nn.Module):
 
         for i, a in enumerate(self.attention_mask):
             embeddings[i] = embeddings[i][:, a == 1]
-            
+
         hidden_layer = -1
         for i in range(len(embeddings)):
             embeddings[i] = embeddings[i][hidden_layer]
@@ -137,11 +165,11 @@ class GenAB(nn.Module):
         attentions = outputs.attentions
         attentions = torch.stack(attentions, dim=1)
         attentions = list(attentions)
-                
+
         for i, a in enumerate(self.attention_mask):
             attentions[i] = attentions[i][:, :, a == 1]
             attentions[i] = attentions[i][:, :, :, a == 1]
-            
+
         embeddings = [e[1:-1].unsqueeze(0) for e in embeddings]
         attentions = [a[:, :, 1:-1, 1:-1].unsqueeze(0) for a in attentions]
 
@@ -152,53 +180,71 @@ class GenAB(nn.Module):
             template_mask=None,
             return_embeddings=True,
         )
-        
+
         model_outs, scores = [], []
         for i, model in enumerate(self.models):
             model_out = model(model_in)
             # model_out = model.gradient_refine(model_in, model_out)
             scores.append(model_out.prmsd.quantile(0.9))
             model_outs.append(model_out)
-                
+
         best_model_i = scores.index(min(scores))
         model_out = model_outs[best_model_i]
 
         return model_out.coords, outputs.species_logits, log_likelyhood
 
+
 class GradientOptimizer:
-    def __init__(self, start: FVbinder, target: FVbinder, target_class=1, lr=0.05, sub_infinity=10, verbose=False, tensorboard=False):
+    def __init__(
+        self,
+        start: FVbinder,
+        target: FVbinder,
+        target_class=1,
+        lr=0.05,
+        sub_infinity=10,
+        verbose=False,
+        tensorboard=False,
+    ):
         self.start = start
         self.target = target
         assert self.target.coords is not None
 
         self.target_class = target_class
 
-    
         self.verbose = verbose
         self.tensorboard = tensorboard
-        
+
         self.generator = GenAB(b=self.start, sub_infinity=sub_infinity)
         self.optimizer = torch.optim.Adam([self.generator.logits], lr=lr)
 
         if self.tensorboard:
             self.writer = SummaryWriter()
-    
+
         self.loss_idx_value = 0
         self.best_valid_binder = None
         self.best_valid_binder_score = 1e12
 
-
-        
     def step(self, steps=1):
         for i in (pbar := tqdm(range(steps))):
             self.optimizer.zero_grad()
             coords, species, log_likelyhood = self.generator.forward()
-            
-            rmsd_loss = RMSD_loss_fn(target=self.target.coords, preds=coords, mask=torch.concat((self.target.vh_cdr_mask, self.target.vl_cdr_mask)))
-            species_loss = species_loss_fn(species, torch.full(size=(species.size(dim=0),), fill_value=self.target_class).to(device))
+
+            rmsd_loss = RMSD_loss_fn(
+                target=self.target.coords,
+                preds=coords,
+                mask=torch.concat((self.target.vh_cdr_mask, self.target.vl_cdr_mask)),
+            )
+            species_loss = species_loss_fn(
+                species,
+                torch.full(
+                    size=(species.size(dim=0),), fill_value=self.target_class
+                ).to(device),
+            )
             log_likelyhood_loss = -log_likelyhood
-        
-            loss = 1 * rmsd_loss + 0.01 * species_loss + 0.3 * log_likelyhood_loss
+
+            # loss = rmsd_loss
+            loss = 10 * rmsd_loss + 0.1 * species_loss + 0.3 * log_likelyhood_loss
+
             # writer.add_scalars("Loss", {
             #     "RMSD Loss": rmsd_loss,
             #     "Species Loss": species_loss,
@@ -206,16 +252,29 @@ class GradientOptimizer:
             #     "Total Loss": loss
             # }, loss_idx_value)
             try:
-                gen_vh, gen_vl = [i.replace(" ", "") for i in self.generator.antiberty_tokenizer.batch_decode(self.generator.logits.argmax(dim=2), skip_special_tokens=True)]
+                gen_vh, gen_vl = [
+                    i.replace(" ", "")
+                    for i in self.generator.antiberty_tokenizer.batch_decode(
+                        self.generator.logits.argmax(dim=2), skip_special_tokens=True
+                    )
+                ]
                 a = FVbinder(vh=gen_vh, vl=gen_vl)
                 valid = True
                 if loss < self.best_valid_binder_score:
                     self.best_valid_binder = a
+
+                if self.tensorboard:
+                    self.writer.add_scalar(
+                        "Valid/VH_v-gen", v_gene_identity(gen_vh), self.loss_idx_value
+                    )
+                    self.writer.add_scalar(
+                        "Valid/VL_v-gen", v_gene_identity(gen_vl), self.loss_idx_value
+                    )
             except Exception as e:
                 valid = False
-            
+
             pbar.set_description(f"Valid: {valid}, Loss {loss}")
-            
+
             if self.verbose:
                 print(f"RMSD Loss: {rmsd_loss}")
                 print(f"Species Loss: {species_loss}")
@@ -223,14 +282,18 @@ class GradientOptimizer:
                 print(f"Full Loss: {loss}")
                 print()
                 print("---")
-            
+
             if self.tensorboard:
                 self.writer.add_scalar("Loss/RMSD Loss", rmsd_loss, self.loss_idx_value)
-                self.writer.add_scalar("Loss/Species Loss", species_loss, self.loss_idx_value)
-                self.writer.add_scalar("Loss/Log-Likelyhood Loss", log_likelyhood_loss, self.loss_idx_value)
+                self.writer.add_scalar(
+                    "Loss/Species Loss", species_loss, self.loss_idx_value
+                )
+                self.writer.add_scalar(
+                    "Loss/Log-Likelyhood Loss", log_likelyhood_loss, self.loss_idx_value
+                )
                 self.writer.add_scalar("Loss/Total Loss", loss, self.loss_idx_value)
-            
+
             loss.backward()
             self.optimizer.step()
-            self.loss_idx_value += 1 
+            self.loss_idx_value += 1
             # print(m.logits)
--- utils/general.py
+++ utils/general.py
@@ -1,54 +1,51 @@
 import os
+from glob import glob
 from typing import List, Tuple
 
+import antiberty
+import igfold
 import numpy as np
-
 import torch
 import torch.nn as nn
 import transformers
-
 from abnumber import Chain
-
-import antiberty
 from antiberty import AntiBERTy
-
-from glob import glob
-import igfold
-
 from igfold.model.IgFold import IgFold
-from igfold.utils.folding import fold
-from igfold.utils.embed import embed
-from igfold.utils.general import exists
-
 from igfold.model.interface import IgFoldInput
+from igfold.utils.embed import embed
 from igfold.utils.fasta import get_fasta_chain_dict
+from igfold.utils.folding import fold
 from igfold.utils.general import exists
-from igfold.utils.pdb import get_atom_coords, save_PDB, write_pdb_bfactor, cdr_indices
+from igfold.utils.pdb import cdr_indices, get_atom_coords, save_PDB, write_pdb_bfactor
 
 project_path = os.path.dirname(os.path.realpath(antiberty.__file__))
-trained_models_dir = os.path.join(project_path, 'trained_models')
-ANTIBERTY_CHECKPOINT_PATH = os.path.join(trained_models_dir, 'AntiBERTy_md_smooth')
-ANTIBERTY_VOCAB_FILE = os.path.join(trained_models_dir, 'vocab.txt')
+trained_models_dir = os.path.join(project_path, "trained_models")
+ANTIBERTY_CHECKPOINT_PATH = os.path.join(trained_models_dir, "AntiBERTy_md_smooth")
+ANTIBERTY_VOCAB_FILE = os.path.join(trained_models_dir, "vocab.txt")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 def exists(x):
     return x is not None
+
 
 def freeze(model):
     for p in model.parameters():
         p.requires_grad = False
 
+
 class FVbinder:
-    def __init__(self,
-                 vh: str, 
-                 vl: str, 
-                 coords: torch.Tensor = None, 
-                 vh_cdr_mask: torch.Tensor = None, 
-                 vl_cdr_mask: torch.Tensor = None
-                ):
-        self.vh = vh
-        self.vl = vl
+    def __init__(
+        self,
+        vh: str,
+        vl: str,
+        coords: torch.Tensor = None,
+        vh_cdr_mask: torch.Tensor = None,
+        vl_cdr_mask: torch.Tensor = None,
+    ):
+        self.vh = Chain(vh, scheme="chothia")
+        self.vl = Chain(vl, scheme="chothia")
         self.coords = coords
         self.vh_cdr_mask = vh_cdr_mask
         if self.vh_cdr_mask is None:
@@ -58,19 +55,18 @@ class FVbinder:
             self.vl_cdr_mask = self.get_mask_from_sequence(self.vl).to(device)
 
     @staticmethod
-    def get_mask_from_sequence(sequence: str) -> torch.Tensor:
-        mask = torch.zeros((len(sequence), 5))
-        a = Chain(sequence, scheme="chothia")
-        for reg, positions in a.regions.items():
-            if reg.startswith("FR"):
-                continue
-            for pos, amino in positions.items():
-                mask[pos.number] = torch.ones(5)
+    def get_mask_from_sequence(chain: Chain) -> torch.Tensor:
+        mask = list()
+        for pos in chain.positions:
+            mask.append(torch.full((5,), fill_value=int(pos.is_in_cdr())))
+        mask = torch.stack(mask).float()
         return mask
 
     def to_pdb(self, filename: str):
-        assert self.coords is not None
-        seqs = [self.vh, self.vl]
+        if self.coords is None:
+            raise ValueError("No coordinates specified. Provide coords or use IgFold")
+
+        seqs = [self.vh.seq, self.vl.seq]
         full_seq = "".join(seqs)
         chains = ["H", "L"]
         delims = np.cumsum([len(s) for s in seqs]).tolist()
@@ -79,57 +75,58 @@ class FVbinder:
             self.coords.squeeze(0),
             full_seq,
             chains=chains,
-            atoms=['N', 'CA', 'C', 'CB', 'O'],
+            atoms=["N", "CA", "C", "CB", "O"],
             # error=res_rmsd,
             delim=delims,
             write_pdb=True,
         )
 
     def __str__(self):
-        return f"FVbinder(\nvh='{self.vh}',\nvl='{self.vl}'\n)"
+        return f"FVbinder(\nvh='{self.vh.seq}',\nvl='{self.vl.seq}'\n)"
 
     def __repr__(self):
-        return self.__str__()
+        return str(self)
 
     def __lt__(self, other):
-        return self.vh < other.vh
+        return str(self) < str(other)
 
-    
-        
+
 ckpt_path = os.path.join(
     os.path.dirname(os.path.realpath(igfold.__file__)),
     "trained_models/IgFold/*.ckpt",
 )
 model_ckpts = list(glob(ckpt_path))
 
+
 class CustomIgFold:
     def __init__(self):
         self.antiberty = AntiBERTy.from_pretrained(ANTIBERTY_CHECKPOINT_PATH).to(device)
         self.antiberty.eval()
-        
-        self.antiberty_tokenizer = transformers.BertTokenizer(vocab_file=ANTIBERTY_VOCAB_FILE, do_lower_case=False)
+
+        self.antiberty_tokenizer = transformers.BertTokenizer(
+            vocab_file=ANTIBERTY_VOCAB_FILE, do_lower_case=False
+        )
 
         self.models = []
         for ckpt_file in model_ckpts:
             print(f"Loading {ckpt_file}...")
             self.models.append(IgFold.load_from_checkpoint(ckpt_file).eval().to(device))
 
-
     def fold(self, binder: FVbinder):
         if exists(binder.coords):
             return
-            
-        seqs = [" ".join(list(i)) for i in [binder.vh, binder.vl]]
+
+        seqs = [" ".join(list(i)) for i in [binder.vh.seq, binder.vl.seq]]
         tokenizer_out = self.antiberty_tokenizer(
-                seqs,
-                return_tensors="pt",
-                padding=True,
+            seqs,
+            return_tensors="pt",
+            padding=True,
         ).to(device)
         attention_mask = tokenizer_out["attention_mask"].to(device)
-        
+
         emb = self.antiberty.bert.embeddings(
-            input_ids = tokenizer_out.input_ids,
-            token_type_ids = tokenizer_out.token_type_ids
+            input_ids=tokenizer_out.input_ids,
+            token_type_ids=tokenizer_out.token_type_ids,
         )
 
         with torch.no_grad():
@@ -138,30 +135,31 @@ class CustomIgFold:
                 inputs_embeds=emb,
                 attention_mask=attention_mask,
                 output_hidden_states=True,
-                output_attentions=True)
-        
+                output_attentions=True,
+            )
+
         embeddings = outputs.hidden_states
         embeddings = torch.stack(embeddings, dim=1)
         embeddings = list(embeddings.detach())
-        
+
         for i, a in enumerate(attention_mask):
             embeddings[i] = embeddings[i][:, a == 1]
-        
+
         hidden_layer = -1
         for i in range(len(embeddings)):
             embeddings[i] = embeddings[i][hidden_layer]
-            
+
         attentions = outputs.attentions
         attentions = torch.stack(attentions, dim=1)
         attentions = list(attentions.detach())
-        
+
         for i, a in enumerate(attention_mask):
             attentions[i] = attentions[i][:, :, a == 1]
             attentions[i] = attentions[i][:, :, :, a == 1]
 
         embeddings = [e[1:-1].unsqueeze(0) for e in embeddings]
         attentions = [a[:, :, 1:-1, 1:-1].unsqueeze(0) for a in attentions]
-        
+
         model_in = IgFoldInput(
             embeddings=embeddings,
             attentions=attentions,
@@ -176,7 +174,7 @@ class CustomIgFold:
                 model_out = model.gradient_refine(model_in, model_out)
                 scores.append(model_out.prmsd.quantile(0.9))
                 model_outs.append(model_out)
-        
+
         best_model_i = scores.index(min(scores))
         model_out = model_outs[best_model_i]
 
